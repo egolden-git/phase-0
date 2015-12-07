5c0de703a4f4f3560f3b67a2f0503fc1eeebdf04
Everett Golden <everett@cloud2spec.com>
2015-12-07 02:30:59 -0800

    update files

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -12,3 +12,10 @@ number = gets.chomp
 num_integer = number.to_i
 num = num_integer + 1
 print "This is a bigger and better number " + num.to_s + "!"
+
+#How do you define a local variable? A local variable is defined by creating a name and assigning a value to it either using an '=' sign or as arguments to a method.
+#How do you define a method? A method is defined using requied argument 'def'.  Each method will start with 'def' and finish with 'end'.
+#What is the difference between a local variable and a method? A local variable is an object which is assigned a value while a method executes operations to a local variable.
+#How do you run a ruby program from the command line? By typing 'ruby <file path>' on the command line.
+#How do you run an RSpec file from the command line? By typing 'rspec <file path to rspec file' on the command line.  RSpec requires RSpec file to execute and validate code.
+#What was confusing about this material? What made sense? The confusing part was the errors and trying to decipher what was happening.
